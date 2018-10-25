b938225ef32d057eec8541ef51e07cefcc57eee7
Wojciech Teichert <wojtek@WojtekPC>
2018-10-25 15:09:42 +0200

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 18 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#18 SMP PREEMPT Thu Oct 25 14:48:53 CEST 2018"
+#define LINUX_COMPILE_BY "wojtek"
+#define LINUX_COMPILE_HOST "WojtekPC"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
